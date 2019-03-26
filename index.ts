b61ec5d4ba492c813ef93549608ceaf0d88e27f5
Asa Jones <thinkinfractals@gmail.com>
2019-03-25 22:47:40 -0400

    Export Generic type AsyncReturnType (Like ReturnType)

--- index.ts
+++ index.ts
@@ -5,3 +5,5 @@ export { Application } from './src/Application'
 export { Route } from './src/Route'
 export { Logger } from './src/Logger'
 export { Env, loadEnv } from 'eznv'
+
+export type AsyncReturnType<T extends (...args: any[]) => any> = T extends (...args: any[]) => Promise<infer R> ? R : ReturnType<T>
