914e00860932c6bf8f4341c25491b8a9d4eb8b58
Asa Jones <thinkinfractals@gmail.com>
2019-03-25 22:27:53 -0400

    Remove Logger interface and guard

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,20 +1,3 @@
-export interface Logger {
-  debug (...args: any[]) : any
-  info (...args: any[]) : any
-  warn (...args: any[]) : any
-  error (...args: any[]) : any
-}
-
-export function isLogger (x: any) : x is Logger {
-  if (!x) return false
-  if (!(x instanceof Object)) return false
-  if (!(x.debug instanceof Function)) return false
-  if (!(x.info instanceof Function)) return false
-  if (!(x.warn instanceof Function)) return false
-  if (!(x.error instanceof Function)) return false
-  return true
-}
-
 export class Logger {
   debug (message: string) {
     console.log(`debug: ${message}`)
