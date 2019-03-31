cb7cb8ea32770977d045a0ee4049af6081607a1a
Asa Jones <thinkinfractals@gmail.com>
2019-03-31 16:37:06 -0400

    Remove eznv and logger as exports
    These don't belong in odayo

--- index.ts
+++ index.ts
@@ -3,7 +3,3 @@ import 'source-map-support/register'
 export { Bootstrap } from './src/Bootstrap'
 export { Application } from './src/Application'
 export { Route } from './src/Route'
-export { Logger } from './src/Logger'
-export { Env, loadEnv } from 'eznv'
-
-export type AsyncReturnType<T extends (...args: any[]) => any> = T extends (...args: any[]) => Promise<infer R> ? R : ReturnType<T>
